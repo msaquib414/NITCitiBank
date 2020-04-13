55c411529641fff9ec5bb8bf80ceb133274d06d1
dev2 <dev2@gmail.com>
2020-04-13 16:17:26 +0530

    vgjfg

--- DemoProj1/src/com/nt/test/Test.java
+++ DemoProj1/src/com/nt/test/Test.java
@@ -2,7 +2,7 @@ package com.nt.test;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws CloneNotSupportedException {
 		//  kyu comment lagaya
 		Test t = new Test();
 		t.clone();
