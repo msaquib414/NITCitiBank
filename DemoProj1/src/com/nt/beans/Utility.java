3a482a5b896467df01d807c5cf20a168f5c73bc5
dev2 <dev2@gmail.com>
2020-04-13 15:00:51 +0530

    modified by dev1

--- DemoProj1/src/com/nt/beans/Utility.java
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -1,8 +1,8 @@
 package com.nt.beans;
 
 import java.util.Date;
-//fthryujfjhfghtyurftu
-public class Utility {
+//i have modified it by dev1
+class Utility {
 	public Date getSysDate() {
 		return new Date();
 	}
